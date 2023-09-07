d71973c1295c0d7c430388b49fd1c0eecfde5b87
Davide Cologgi <dcologgi@e4r9p1.42roma.it>
2023-09-07 08:44:18 +0200

    exam04

---
+++ microshell.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   microshell.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcologgi <dcologgi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/04 13:43:58 by dcologgi          #+#    #+#             */
+/*   Updated: 2023/09/04 13:46:11 by dcologgi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MICROSHELL_H
+# define MICROSHELL_H
+
+# include <unistd.h>
+# include <string.h>
+# include <sys/wait.h>
+
+#endif
